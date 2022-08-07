dc4db9ae36a296863285b1b41a0d4c7d6432a601
zhanghaoran <2017929959@qq.com>
2022-08-07 10:50:40 +0800

    ""

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,8 +1,10 @@
 import com.alibaba.druid.pool.DruidDataSource;
 import com.zhr.springhello.pojo.Class;
 import com.zhr.springhello.pojo.Student;
+import com.zhr.springhello.pojo.User;
 import javafx.scene.control.TableColumn;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import java.sql.SQLException;
@@ -42,6 +44,28 @@ public class Test {
 //        Student student = ioc.getBean("student", Student.class);
 //        Student student1 = ioc.getBean("student", Student.class);
 //        System.out.println(student.equals(student1));
+
+
+        /**
+         * ConfigurableApplicationContext是applicationContext的子接口，其中扩展了刷新和关闭的方法
+         * 1.实例化
+         * 2，依赖注入
+         * 3，初始化，需要需要通过bean的init-method
+         * 4，IOC容器关闭的时候销货，需要通过bean的destroy-method属性指定方法
+         *
+         * 注意
+         * 若bean的作用域为单例时，生命周期的前三个步骤，会在获取ioc容器的时候执行，
+         * 若bean的作用域为多例的时候，生命周期的前三个步骤，会在获取bean的时候执行.
+         * */
+
+        ConfigurableApplicationContext classPathXmlApplicationContext = new ClassPathXmlApplicationContext("spring-lifecircle.xml");
+        User user = classPathXmlApplicationContext.getBean("user", User.class);
+        System.out.println(user);
+        classPathXmlApplicationContext.close();
+
+
+
+
     }
 
 
